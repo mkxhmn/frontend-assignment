fe4839e1b7b04b8423911569e8f675e28f856fae
Farid Nikman <fnikman@gmail.com>
2023-08-04 06:37:38 +0800

    feat: implement mock link for header and footer

--- src/components/shared/Footer/index.tsx
+++ src/components/shared/Footer/index.tsx
@@ -1,3 +1,22 @@
-export const Footer = () => {
-  return <footer>hello footer</footer>;
+import { MenuLink } from "@/components/shared/MenuLink";
+
+type FooterProps = {
+  links?: MenuLink[];
+};
+
+export const Footer = (props: FooterProps) => {
+  return (
+    <footer className="max-w-container relative bottom-0 mx-auto mt-32 w-full px-4 sm:px-6 lg:px-8">
+      <div className="py-10">
+        <p className="mt-5 text-center text-sm leading-6 text-slate-500">
+          Where customers become lifelong partners.
+        </p>
+        {props?.links && (
+          <div className="mt-12 flex items-center justify-center">
+            <MenuLink links={props.links} />
+          </div>
+        )}
+      </div>
+    </footer>
+  );
 };
