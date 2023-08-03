5b49b06ec6b1f8204824e59e4c0310633b07dd38
Farid Nikman <fnikman@gmail.com>
2023-08-04 04:25:40 +0800

    feat: metadata for login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,12 @@
 import { LoginForm } from "@/app/(auth)/_form";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login • Customer Portal",
+  description:
+    "Welcome back to customer portal! Unveil your customer army here and conquer the world with outstanding service. It's customer appreciation day, every day",
+};
 
 export default function Login() {
-  return (
-    <div>
-      <LoginForm />
-    </div>
-  );
+  return <LoginForm />;
 }
